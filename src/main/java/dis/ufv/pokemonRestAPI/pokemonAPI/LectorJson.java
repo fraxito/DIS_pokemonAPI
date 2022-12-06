e48e361a9065d3f4fad8fbe095de3904af25eb8b
Jorge Cisneros <fraxito@gmail.com>
2022-12-06 17:00:34 +0100

    API REST Finalizada

---
+++ src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/LectorJson.java
@@ -0,0 +1,26 @@
+package dis.ufv.pokemonRestAPI.pokemonAPI;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class LectorJson {
+    public ArrayList<Pokemon> leeFicheroJson(String fichero){
+        try {
+            //lee el fichero que le pasamos y lo carga en un reader
+            Reader reader = Files.newBufferedReader(Paths.get(fichero));
+            // convierte el array JSON a un arraylist de users
+            ArrayList<Pokemon> listaPokemon =
+                    new Gson().fromJson(reader, new TypeToken<ArrayList<Pokemon>>() {}.getType());
+            reader.close();// close reader
+            return listaPokemon;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return new ArrayList<>(); //si no ha leido nada, devuelve un array vacio
+        }
+    }
+}
